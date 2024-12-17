c73b605aef31d7b04838653e1d3a37f38e4d7550
Quoc-Hwng <phamhung151020@gmail.com>
2024-12-17 15:02:30 +0700

    middleware

--- src/app/(public)/nav-items.tsx
+++ src/app/(public)/nav-items.tsx
@@ -1,30 +1,42 @@
 "use client";
 
+import { getAccessTokenFromLocalStorage } from "@/lib/utils";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const menuItems = [
   {
     title: "Món ăn",
-    href: "/menu",
+    href: "/menu", //authRequired = undefined : đăng nhập hay chưa vẫn hiển thị
   },
   {
     title: "Đơn hàng",
     href: "/orders",
+    authRequired: true,
   },
   {
     title: "Đăng nhập",
     href: "/login",
-    authRequired: false,
+    authRequired: false, //Hiển thị khi chưa đăng nhập
   },
   {
     title: "Quản lý",
     href: "/manage/dashboard",
-    authRequired: true,
+    authRequired: true, //Hiển thị khi đã đăng nhập
   },
 ];
 
 export default function NavItems({ className }: { className?: string }) {
+  const [isAuth, setIsAuth] = useState(false);
+  useEffect(() => {
+    setIsAuth(Boolean(getAccessTokenFromLocalStorage()));
+  }, []);
   return menuItems.map((item) => {
+    if (
+      (item.authRequired === false && isAuth) ||
+      (item.authRequired === true && !isAuth)
+    )
+      return null;
     return (
       <Link href={item.href} key={item.href} className={className}>
         {item.title}
---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const privatePaths = ["/manage"];
+const unAuthPaths = ["/login"];
+
+//This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  //Chưa login thì không cho vào private paths
+  const isAuth = Boolean(request.cookies.get("accessToken")?.value);
+  if (privatePaths.some((path) => pathname.startsWith(path)) && !isAuth) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  //Nếu login thì không cho vào login nữa
+  if (unAuthPaths.some((path) => pathname.startsWith(path)) && isAuth) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  return NextResponse.next();
+}
+
+//See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/manage/:path*", "/login"],
+};
